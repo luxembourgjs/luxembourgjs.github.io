7a58afac8ec7a8bab503418fec52695880be3029
Dominik Nowak <dnowak94@live.de>
2025-04-14 20:56:31 +0200

    refactor: load presentation json file once, load avatar using https://github.com/{username}.png

--- data/load_data.js
+++ data/load_data.js
@@ -1,26 +1,8 @@
-import { fetchGithubProfile } from "../api/github.js";
-async function load_authors(talk) {
-    let fetch_authors_promises = [];
-    talk.authors.forEach(author => {
-        fetch_authors_promises.push(fetchGithubProfile(author));
-    });
-
-    talk.authors = await Promise.all(fetch_authors_promises);
-    return talk;
-}
-
-export async function load_data() {
+export async function load_presentation_data() {
     try {
         const data = await fetch('data/pres-data.json');
         const pres_data = await data.json();
-        let result = { slides: []};
-        let fetch_result = [];
-        
-        pres_data.slides.forEach((talk, index) => {
-            fetch_result.push(load_authors(talk));
-        });
-        result['slides'] = await Promise.all(fetch_result);
-        return result;
+        return pres_data;
     }
     catch(error) {
         console.error('Error loading talks:', error);
